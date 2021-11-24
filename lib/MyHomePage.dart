fc43db23d63f259d011267bc3dcba4969cd2198b
Brandon <brandon_jipiu@yahoo.com>
2021-11-24 23:42:33 +0100

    Create a card text style
    -Style iso be used in the dashboard cards

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -28,6 +28,13 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+
+    // style
+    var cardTextStyle = const TextStyle(
+        fontFamily: "Montserrat Regular",
+        fontSize: 14,
+        color: Color.fromRGBO(63, 63, 63, 1));
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
