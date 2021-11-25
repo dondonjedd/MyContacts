11768be0a0bbcfd0c0a621d4413e54be150bfce7
Brandon <brandon_jipiu@yahoo.com>
2021-11-25 20:15:39 +0100

    Display the contacts in a list view
    -bug: in chrome, it shows a red interface for a while before displaying contacts

--- lib/ContactScreen.dart
+++ lib/ContactScreen.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+import 'package:flutter/material.dart';
 
 class ContactScreen extends StatefulWidget {
   const ContactScreen({Key? key}) : super(key: key);
@@ -10,6 +11,27 @@ class ContactScreen extends StatefulWidget {
 class _ContactScreenState extends State<ContactScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text("All Contacts"),),
+      body: Center(
+        child: FutureBuilder(
+          future: DefaultAssetBundle.of(context).loadString("assets/files/AllContacts.json"),
+          builder: (context, snapshot){
+          var showData=json.decode(snapshot.data.toString());
+          return ListView.builder(
+            itemBuilder: (BuildContext context, int index){
+              return ListTile(
+                title: Text(showData[index]['user']),
+                subtitle: Text(showData[index]['phone']),
+              );
+            },
+            itemCount: showData.length,
+          );
+        },
+
+        ),
+      ),
+
+    );
   }
 }
