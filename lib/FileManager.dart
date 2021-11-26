f3b118141dd23d733202ba4e34325b928dd7d09a
Brandon <brandon_jipiu@yahoo.com>
2021-11-27 00:25:44 +0100

    import path provider
    -To read/write json files

--- lib/FileManager.dart
+++ lib/FileManager.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
-import 'dart:typed_data';
+import 'package:flutter/cupertino.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'package:intl/intl.dart';
 import 'package:flutter/services.dart';
