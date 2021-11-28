930e398cebd9a620c4e32df38deb43a672683ead
Brandon <brandon_jipiu@yahoo.com>
2021-11-28 18:24:15 +0100

    Create splash screen using vimigo's logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'dashboard.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -12,10 +12,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       title: 'My Contacts',
+      home:AnimatedSplashScreen(
+          duration: 3000,
+          splash: SvgPicture.asset('assets/images/vimigo.svg',),
+          nextScreen: const DashBoard(title: 'Main Menu'),
+          splashTransition: SplashTransition.fadeTransition,
+          backgroundColor: Colors.blue
+      )
 
-      home: DashBoard(title: 'Main Menu'),
     );
   }
 }
