6693f540aeccb9f9ab0edf1e01b6efeb241c818f
Brandon <brandon_jipiu@yahoo.com>
2021-11-28 17:17:25 +0100

    Create about screen appBar

--- lib/about_screen.dart
+++ lib/about_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:mycontacts/themes.dart';
 
 class AboutScreen extends StatefulWidget {
   const AboutScreen({Key? key}) : super(key: key);
@@ -8,8 +10,23 @@ class AboutScreen extends StatefulWidget {
 }
 
 class _AboutScreenState extends State<AboutScreen> {
+
+  final ThemeData _light = Themes().getThemeLight();
+  final ThemeData _dark = Themes().getThemeDark();
+  var _isDark=false;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+        darkTheme:_dark,
+        theme: _light,
+        themeMode: _isDark ? ThemeMode.dark : ThemeMode.light,
+        home:Scaffold(
+          appBar: AppBar(title: const Text("About",style: TextStyle(fontFamily: 'DancingScript'))),
+          body: Card(
+            margin: const EdgeInsets.all(10),
+            child:Text("test")
+          )
+        )
+    );
   }
 }
