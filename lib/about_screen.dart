ca3f6523c0e4f494f2e0681c52480f087b291687
Brandon <brandon_jipiu@yahoo.com>
2021-11-28 17:43:17 +0100

    Update the information displayed in about screen
    -interface was modified
    -text was updated

--- lib/about_screen.dart
+++ lib/about_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:mycontacts/themes.dart';
 
 class AboutScreen extends StatefulWidget {
@@ -22,9 +23,29 @@ class _AboutScreenState extends State<AboutScreen> {
         themeMode: _isDark ? ThemeMode.dark : ThemeMode.light,
         home:Scaffold(
           appBar: AppBar(title: const Text("About",style: TextStyle(fontFamily: 'DancingScript'))),
-          body: Card(
-            margin: const EdgeInsets.all(10),
-            child:Text("test")
+          body: Container(
+            alignment: Alignment.topCenter,
+            child:Card(shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)), elevation: 10,
+                margin: const EdgeInsets.all(10),
+                child:Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Text("Developed by : Brandon Jedd Arius Jipiu\n"),
+                        Text("\nDescription:\nVimigo's candidate technical assessment to asses the candidate's skill"),
+                        Text("\nPlugins used:\n1. cupertino_icons\n2.flutter_svg \n3.flutter_staggered_grid_view"
+                            " \n4.flutter_staggered_animations \n5.intl \n6.fluttertoast \n7.shared_preferences \n8.username_gen"
+                            " \n9.path_provider \n10.share ")
+
+
+
+
+                      ]),
+                )
+            )
           )
         )
     );
