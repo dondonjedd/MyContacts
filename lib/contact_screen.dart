3fc61eca63764a01d7ed69451c87d12c2559e2e3
Brandon <brandon_jipiu@yahoo.com>
2021-11-30 19:41:59 +0100

    Change colors of list Tiles and share button in dark mode

--- lib/contact_screen.dart
+++ lib/contact_screen.dart
@@ -148,9 +148,11 @@ class _ContactScreenState extends State<ContactScreen> {
                                       margin: const EdgeInsets.all(10),
                                       child: ListTile(
                                         leading: Text((index+1).toString()+'. '+(snapshot.data as List)[index]["user"]),
-                                        title: Text((snapshot.data as List)[index]["phone"]),tileColor: Colors.blueGrey.shade50,
+                                        title: Text((snapshot.data as List)[index]["phone"]),tileColor:_isDark? Colors.grey.shade800:Colors.blueGrey.shade50,
                                         subtitle: isSelected[0]?Text((snapshot.data as List)[index]["date"] +'\t\t'+(snapshot.data as List)[index]["time"]) :Text((snapshot.data as List)[index]["timeAgo"]),
-                                        trailing: IconButton(onPressed: () { _onShare(context,snapshot.data as List,index); }, icon: SvgPicture.asset('assets/images/shareBtn.svg',height: 15,color: Colors.blue.shade300))
+                                        trailing: IconButton(
+                                            onPressed: () {_onShare(context,snapshot.data as List,index); },
+                                            icon: SvgPicture.asset('assets/images/shareBtn.svg',height: 15,color:_isDark?Colors.grey: Colors.blue.shade300))
                                       ),
                                     ),
                                   )
